8ba82317907d7ab98be27367fa2c9613b2680fa7
MrPeterss <86176234+MrPeterss@users.noreply.github.com>
2023-06-04 21:29:04 -0400

    size fix

--- app/page.js
+++ app/page.js
@@ -3,8 +3,8 @@ import styles from './page.module.css'
 
 export default function Home() {
   return (
-    <main>
-      <iframe src="https://www.google.com/maps/d/u/0/embed?mid=1F02yuT8lj229-C8vMS24-Z3wHKk9G9k&ehbc=2E312F" width="640" height="480"></iframe>
+    <main className=''>
+      <iframe src="https://www.google.com/maps/d/u/0/embed?mid=1F02yuT8lj229-C8vMS24-Z3wHKk9G9k&ehbc=2E312F" width="" height=""></iframe>
     </main>
   )
 }
