f4051d035daa44ee90622ffbaf4e48afda51a213
MrPeterss <86176234+MrPeterss@users.noreply.github.com>
2023-06-04 21:26:43 -0400

    Update

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ import styles from './page.module.css'
 
 export default function Home() {
   return (
-    <main className={styles.main}>
+    <main>
       <iframe src="https://www.google.com/maps/d/u/0/embed?mid=1F02yuT8lj229-C8vMS24-Z3wHKk9G9k&ehbc=2E312F" width="640" height="480"></iframe>
     </main>
   )
