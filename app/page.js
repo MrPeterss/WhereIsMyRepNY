a8dd4f5f7305f2479aa93d2ae062b2ca00140636
MrPeterss <86176234+MrPeterss@users.noreply.github.com>
2023-06-06 15:34:40 -0400

    Update Favicon and Site Name

--- app/page.js
+++ app/page.js
@@ -1,9 +1,13 @@
 import Image from 'next/image'
 import styles from './page.module.css'
+import Head from 'next/head'
 
 export default function Home() {
   return (
     <main className={styles.main}>
+      <Head>
+        <link rel="shortcut icon" href="/" />
+      </Head>
       <iframe src="https://www.google.com/maps/d/u/0/embed?mid=1F02yuT8lj229-C8vMS24-Z3wHKk9G9k&ehbc=2E312F" width="640" height="480"></iframe>
     </main>
   )
